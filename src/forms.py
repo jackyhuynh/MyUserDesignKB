6888bb0a23340d68ff1dc138b763c28de8a9bebf
trucdev <jackyhuynh87@gmail.com>
2024-10-02 16:34:40 -0500

    make update to remove the existing CI/CD Pipelines

--- src/forms.py
+++ src/forms.py
@@ -1,7 +1,7 @@
+from flask_ckeditor import CKEditorField
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
-from flask_ckeditor import CKEditorField
 
 
 ##WTForm
--- src/main.py
+++ src/main.py
@@ -1,20 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, flash, abort
+from flask import Flask, render_template, redirect, flash
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
-from datetime import date
-from functools import wraps
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from src.forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
+from flask_login import UserMixin, login_user, logout_user
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
-gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
+gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
+                    base_url=None)
 
 
 @app.route('/')
